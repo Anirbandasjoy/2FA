e62b0c25296f396053b5123a9107dd39351711eb
Joy Das <joy600508@gmail.com>
2024-12-24 13:12:28 +0600

    i dont know

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,18 @@ import cookieParser from "cookie-parser";
 
 const app: Application = express();
 app.use(morgan("dev"));
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://authnexus.vercel.app"],
+    origin: ["https://authnexus.vercel.app"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
